b496ac0db383d96819c46e2d77ceef6d55ce6688
pranitakawadkar28 <pranitakawadkar28@gmail.com>
2025-09-10 21:00:21 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,34 @@
-import express from "express"
-import { ENV } from './config/env.js'
-import connectDb from './config/db.js'
+import express from "express";
+import { ENV } from "./config/env.js";
+import connectDb from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Hello World!!!")
-})
+app.use(express.json());
+app.use(clerkMiddleware());
 
-app.listen(ENV.PORT, () => {
-    console.log(`server is running on ${ENV.PORT}`);
-    connectDb()
-})
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+const startServer = async () => {
+  try {
+    await connectDb();
+    if (ENV.NODE_ENV !== "production")
+      app.listen(ENV.PORT, () => {
+        console.log(`server is running on ${ENV.PORT}`);
+      });
+  } catch (error) {
+    console.error("Error starting server", error)
+    process.exit(1)
+  }
+};
+
+startServer();
 
 export default app;
