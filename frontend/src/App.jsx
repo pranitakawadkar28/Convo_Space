8d49b91c438095102b5d83192cf30048d35e1cff
pranitakawadkar28 <pranitakawadkar28@gmail.com>
2025-09-21 14:41:57 +0530

    AuthProvider and tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,25 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-reac
 import { Routes, Route, Navigate } from 'react-router';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import * as Sentry from "@sentry/react";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
   return (
     <>
       <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path='/' element={<Home/>}/>
           <Route path='/auth' element={<Navigate to={"/"} replace/>}/>
-        </Routes>
+        </SentryRoutes>
       </SignedIn>
 
       <SignedOut>
-        <Routes>
+        <SentryRoutes>
           <Route path='/auth' element={<Auth/>}/>
           <Route path='*' element={<Navigate to={"/auth"} replace/>}/>
-        </Routes>
+        </SentryRoutes>
       </SignedOut>
     </>
   )
